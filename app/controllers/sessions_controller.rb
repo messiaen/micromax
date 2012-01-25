23753c76ef605903c7c3f49af5ddf19eb4d348f8
Greg Clark <greg@oscili>
2012-01-24 23:52:36 -0500

    -fixed redirects if admin is logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,14 @@ class SessionsController < ApplicationController
     return_to = session[:return_to]
     
     if Person.authenticate(params[:person][:username], params[:person][:password])
-      session[:user_id] = Person.find_by_username(params[:person][:username]).id
+	  person = Person.find_by_username(params[:person][:username])
+      session[:user_id] = person.id
       
-      redirect_to return_to || accounts_path
+	  if person.is_admin? 
+        redirect_to return_to || "/admin"
+	  else
+	    redirect_to return_to || accounts_path
+	  end
     else
       flash[:error] = "Invalid Username / Password"
       redirect_to :root
